4c776ad00cdf35415d30608a0cfbbf13136c827d
shraddhamira <shraddha.s.mirashi@gmail.com>
2018-11-05 08:53:09 +0530

    New

---
+++ src/main/java/com/pojo/Expense.java
@@ -0,0 +1,71 @@
+package com.pojo;
+
+import java.util.Date;
+
+public class Expense {
+
+	private long id;
+	private String description;
+	private Date createDate;
+	private long userId;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	@Override
+	public String toString() {
+		return "Expense [id=" + id + ", description=" + description + ", createDate=" + createDate + ", userId="
+				+ userId + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Expense other = (Expense) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+}
